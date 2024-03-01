84235d1701bf89c2146bfb8bc6ddc9ee812ff544
Douglas Dias <douglasdias.souza@outlook.com>
2024-03-01 19:20:24 -0300

    Algoritmo atualizado.

---
+++ exercicio_4_ia_expert_academy.py
@@ -0,0 +1,9 @@
+# Exercício 4: Ia Expert Academy
+
+distancia = float(input("Qual a distância percorrida: "))
+velocidade_media = float(input("Qual a velocidade média: "))
+
+distancia = (distancia * velocidade_media)
+
+litros_usados = distancia / 12
+print(f"Foi consumido {litros_usados} litros")
