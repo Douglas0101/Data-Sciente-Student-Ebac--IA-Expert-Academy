a9596fac90ce956768663f87ed8d85d915253929
Douglas Dias <douglasdias.souza@outlook.com>
2024-03-02 00:04:36 -0300

    Update exercicio_4_ia_expert_academy.py

--- exercicio_4_ia_expert_academy.py
+++ exercicio_4_ia_expert_academy.py
@@ -1,9 +1,10 @@
 # Exercício 4: Ia Expert Academy
 
 distancia = float(input("Qual a distância percorrida: "))
+tempo_gasto = int(input("Qual o tempo gasto(em minutos): "))
 velocidade_media = float(input("Qual a velocidade média: "))
 
-distancia = (distancia * velocidade_media)
+distancia = (tempo_gasto * velocidade_media)
 
 litros_usados = distancia / 12
-print(f"Foi consumido {litros_usados} litros")
+print(f"Foi consumido {litros_usados:.2f} litros")
