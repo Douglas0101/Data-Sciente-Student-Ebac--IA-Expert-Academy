a6dfbb52577606d3572b561249389338d3b6fa26
Douglas Dias <douglasdias.souza@outlook.com>
2024-04-16 15:04:35 -0300

    Estudos e pesquisa Data Science
    ...

---
+++ dados.py
@@ -0,0 +1,12 @@
+# Módulo de leitura de dados
+
+def ler_dados(dados):
+    print(dados)
+    dados = input()
+    return dados
+
+
+def dados_float(num_float):
+    print(num_float)
+    num_float = input(float())
+    return float(num_float)
